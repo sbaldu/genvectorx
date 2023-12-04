bd404e45ef91793095f28821269176b85b0f2d08
mdessole <monicadessole@gmail.com>
2023-12-04 10:55:42 +0100

    added arg to runtests

--- plots/runtests.sh
+++ plots/runtests.sh
@@ -3,10 +3,10 @@
 python /home/mdessole/Projects/ROOT/genvectorxfork/plots/plots.py Boost cpu 3
 python /home/mdessole/Projects/ROOT/genvectorxfork/plots/plots.py InvariantMasses cpu 3
 
-ACPP_VISIBILITY_MASK=ocl ONEAPI_DEVICE_SELECTOR=opencl:0 python /home/mdessole/Projects/ROOT/genvectorxfork/plots/plots.py Boost fpga_buffers 3
-ACPP_VISIBILITY_MASK=ocl ONEAPI_DEVICE_SELECTOR=opencl:1 python /home/mdessole/Projects/ROOT/genvectorxfork/plots/plots.py Boost ocl_buffers 3
-ACPP_VISIBILITY_MASK=cuda ONEAPI_DEVICE_SELECTOR=cuda:0 python /home/mdessole/Projects/ROOT/genvectorxfork/plots/plots.py Boost cuda_buffers 3
+ACPP_VISIBILITY_MASK=ocl ONEAPI_DEVICE_SELECTOR=opencl:0 python /home/mdessole/Projects/ROOT/genvectorxfork/plots/plots.py Boost fpga_$1 3
+ACPP_VISIBILITY_MASK=ocl ONEAPI_DEVICE_SELECTOR=opencl:1 python /home/mdessole/Projects/ROOT/genvectorxfork/plots/plots.py Boost ocl_$1 3
+ACPP_VISIBILITY_MASK=cuda ONEAPI_DEVICE_SELECTOR=cuda:0 python /home/mdessole/Projects/ROOT/genvectorxfork/plots/plots.py Boost cuda_$1 3
 
-ACPP_VISIBILITY_MASK=ocl ONEAPI_DEVICE_SELECTOR=opencl:1 python /home/mdessole/Projects/ROOT/genvectorxfork/plots/plots.py InvariantMasses ocl_buffers 3
-ACPP_VISIBILITY_MASK=ocl ONEAPI_DEVICE_SELECTOR=opencl:0 python /home/mdessole/Projects/ROOT/genvectorxfork/plots/plots.py InvariantMasses fpga_buffers 3
-ACPP_VISIBILITY_MASK=cuda ONEAPI_DEVICE_SELECTOR=cuda:0 python /home/mdessole/Projects/ROOT/genvectorxfork/plots/plots.py InvariantMasses cuda_buffers 3
+ACPP_VISIBILITY_MASK=ocl ONEAPI_DEVICE_SELECTOR=opencl:1 python /home/mdessole/Projects/ROOT/genvectorxfork/plots/plots.py InvariantMasses ocl_$1 3
+ACPP_VISIBILITY_MASK=ocl ONEAPI_DEVICE_SELECTOR=opencl:0 python /home/mdessole/Projects/ROOT/genvectorxfork/plots/plots.py InvariantMasses fpga_$1 3
+ACPP_VISIBILITY_MASK=cuda ONEAPI_DEVICE_SELECTOR=cuda:0 python /home/mdessole/Projects/ROOT/genvectorxfork/plots/plots.py InvariantMasses cuda_$1 3
