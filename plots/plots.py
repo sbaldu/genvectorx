cac6a65e24c6bea3a8e6b35f83fa8b618c481bfd
mdessole <monicadessole@gmail.com>
2024-02-22 10:01:50 +0100

    Add nsys profiling benchmark

--- plots/plots.py
+++ plots/plots.py
@@ -3,12 +3,17 @@ import subprocess
 import math
 import os
 import sys
+import time
 import numpy as np
+from pathlib import Path
 
-buildcuda = '/home/mdessole/Projects/ROOT/genvectorxfork/build_cuda/'
-buildoneapi = '/home/mdessole/Projects/ROOT/genvectorxfork/build_oneapi/'
-buildacpp = '/home/mdessole/Projects/ROOT/genvectorxfork/build_adaptivecpp/'
-path = '/home/mdessole/Projects/ROOT/genvectorxfork/plots/'
+buildcuda = '/home/mdessole/Projects/GenVectorX/genvectorx/build_cuda/'
+buildoneapi = '/home/mdessole/Projects/GenVectorX/genvectorx/build_oneapi/'
+buildacpp = '/home/mdessole/Projects/GenVectorX/genvectorx/build_adaptivecpp/'
+path = '/home/mdessole/Projects/GenVectorX/genvectorx/plots/'
+nsys = "/usr/local/cuda-12.2/bin/nsys"
+
+base_header = "N,env,type,testname,precision"
 
 def result2list(exe, N, nruns):
     print(exe, str(int(N)), str(int(nruns)))
@@ -34,10 +39,14 @@ def result2list(exe, N, nruns):
     #endif
     return time
 
-def run_benchmark(builddir, sizes, nruns = 1, testname = "InvariantMasses", btype = 'cpu'):
+def run_benchmark(builddir, sizes, nruns = 1,                         
+                testname = "InvariantMasses", 
+                precision = 64, btype = 'cpu'):
     timeopt = array( 'd' )
     timestd = array( 'd' )
     
+    if precision == 32:
+        testname = 'S'+testname
     if btype == 'cpu':
         exe = os.path.join(builddir,'testx',testname)
     elif btype == 'cuda':
@@ -60,6 +69,118 @@ def run_benchmark(builddir, sizes, nruns = 1, testname = "InvariantMasses", btyp
     #endfor
     return timeopt, timestd
 
+def run_nsys_benchmark(builddir, sizes, nruns = 1, 
+                        testname = "InvariantMasses", 
+                        memtype = '',
+                        precision = 64,
+                        btype = 'cpu', 
+                        output_file = 'output_file'):
+
+    if precision == 32:
+        testname = 'S'+testname
+    if btype == 'cpu':
+        exe = os.path.join(builddir,'testx',testname)
+        environment = 'CPU'
+    elif btype == 'cuda':
+        exe = os.path.join(builddir,'testx',testname+'CUDA')
+        environment = 'CUDA'
+    elif btype == 'sycl':
+        exe = os.path.join(builddir,'testx',testname+'SYCL')
+        if "oneapi" in builddir.lower():
+            environment = 'oneAPI'
+        if ("acpp" in builddir.lower()) or ("adaptivecpp" in builddir.lower()):
+            environment = 'AdaptiveCPP'
+    else:
+        raise ValueError('Unknown backend')
+    #endif
+    if precision == 32:
+        testname = testname[1:]
+    
+    if (not os.path.exists(exe)):
+        print("Executable", exe, "not found!")
+        return 
+
+            
+    if not os.path.exists(f"{output_file}"):
+        os.mkdir(f"{output_file}")
+    
+    for i, N in enumerate(sizes):
+        time = result2list(exe,N,nruns)
+        arg = f"{int(N)} {nruns}"
+        # Profile code with nsys
+        try:
+            profile = subprocess.run(
+                                    [nsys, "profile", "-otemp", exe, *arg.split()],
+                                    #env={**os.environ, e: "1"},
+                                    stdout=subprocess.PIPE,
+                                    check=True,
+                            )
+        except subprocess.CalledProcessError:
+            continue
+
+
+        # Gather statistics
+        result = subprocess.run(
+            [nsys, "stats", "temp.nsys-rep", "--format=csv"],
+            stdout=subprocess.PIPE,
+            check=True
+        )
+
+        subprocess.run(["rm", "temp.nsys-rep", "temp.sqlite"])
+        output = result.stdout.decode("utf-8").split("\n\n")
+        
+        # If output file does not exist, write headers
+        if not Path(f"{output_file}/api").exists():
+            with open(f"{output_file}/api", "w") as file_handler:
+                header = output[2].split("\n")[1]
+                if 'SKIPPED' in header:
+                    print(output)
+                    quit()
+                file_handler.write(f"{base_header},{header}\n")
+        if not Path(f"{output_file}/kernel").exists():
+            with open(f"{output_file}/kernel", "w") as file_handler:
+                header = output[3].split("\n")[1]
+                file_handler.write(f"{base_header},{header}\n")
+        if not Path(f"{output_file}/memop").exists():
+            with open(f"{output_file}/memop","w") as file_handler:
+                header = output[4].split("\n")[1]
+                file_handler.write(f"{base_header},{header}\n")
+
+        # Write row
+        out_base = f"{int(N)},{environment},{memtype},{testname},{precision}"
+        with open(
+            f"{output_file}/api",
+            "a",
+        ) as file_handler:
+            file_handler.write(
+                "\n".join([f"{out_base},{s}" for s in output[2].split("\n")[2:]])
+            )
+            file_handler.write("\n")
+        with open(
+            f"{output_file}/kernel",
+            "a",
+        ) as file_handler:
+            file_handler.write(
+                "\n".join([f"{out_base},{s}" for s in output[3].split("\n")[2:]])
+            )
+            file_handler.write("\n")
+        with open(
+            f"{output_file}/memop",
+            "a",
+        ) as file_handler:
+            file_handler.write(
+                "\n".join([f"{out_base},{s}" for s in output[4].split("\n")[2:]])
+            )
+            file_handler.write("\n")
+
+        # with open(f"{outpu}/api/N{int(N)}", "w") as file_handler:
+        #     file_handler.write("\n".join(output[2].split("\n")[1:]))
+        # with open(f"{output_file}/kernel/N{int(N)}", "w") as file_handler:
+        #     file_handler.write("\n".join(output[3].split("\n")[1:]))
+        # with open(f"{output_file}/memop/N{int(N)}", "w") as file_handler:
+        #     file_handler.write("\n".join(output[4].split("\n")[1:]))
+    #endfor
+    return 
 
 def collect_results(testname, platform, nruns):
     nruns = int(nruns)
@@ -83,7 +204,7 @@ def collect_results(testname, platform, nruns):
     
     if (platform == 'cpu'):
         timecpu, stdcpu = run_benchmark(buildacpp, sizes, testname = testname, nruns = nruns, btype = 'cpu')
-        Stimecpu, Sstdcpu = run_benchmark(buildacpp, Ssizes, testname = "S"+testname, nruns = nruns, btype = 'cpu')
+        Stimecpu, Sstdcpu = run_benchmark(buildacpp, Ssizes, testname = testname, precision = 32, nruns = nruns, btype = 'cpu')
         savez_dict['timecpu'] = timecpu
         savez_dict['Stimecpu'] = Stimecpu
         savez_dict['stdcpu'] = stdcpu
@@ -91,7 +212,7 @@ def collect_results(testname, platform, nruns):
     else:
         if (platform != "hip"):
             timeoneapi, stdoneapi = run_benchmark(buildoneapi, sizes, testname = testname, nruns = nruns, btype = 'sycl')
-            Stimeoneapi, Sstdoneapi = run_benchmark(buildoneapi, Ssizes, testname = "S"+testname, nruns = nruns, btype = 'sycl')
+            Stimeoneapi, Sstdoneapi = run_benchmark(buildoneapi, Ssizes, testname = testname, precision = 32, nruns = nruns, btype = 'sycl')
             savez_dict['timeoneapi'] = timeoneapi
             savez_dict['Stimeoneapi'] = Stimeoneapi
             savez_dict['stdoneapi'] = stdoneapi
@@ -99,7 +220,7 @@ def collect_results(testname, platform, nruns):
         #endif
 
         timeacpp, stdacpp = run_benchmark(buildacpp, sizes, testname = testname, nruns = nruns, btype = 'sycl')
-        Stimeacpp, Sstdacpp = run_benchmark(buildacpp, Ssizes, testname = "S"+testname, nruns = nruns, btype = 'sycl')
+        Stimeacpp, Sstdacpp = run_benchmark(buildacpp, Ssizes, testname = testname, precision = 32, nruns = nruns, btype = 'sycl')
         savez_dict['timeacpp'] = timeacpp
         savez_dict['Stimeacpp'] = Stimeacpp
         savez_dict['stdacpp'] = stdacpp
@@ -107,7 +228,7 @@ def collect_results(testname, platform, nruns):
 
         if ("cuda" in platform):
             timecuda, stdcuda = run_benchmark(buildcuda, sizes, testname = testname, nruns = nruns, btype = 'cuda')
-            Stimecuda, Sstdcuda = run_benchmark(buildcuda, Ssizes, nruns = nruns, testname = "S"+testname, btype = 'cuda')
+            Stimecuda, Sstdcuda = run_benchmark(buildcuda, Ssizes, nruns = nruns, precision = 32, testname = testname, btype = 'cuda')
             savez_dict['timecuda'] = timecuda
             savez_dict['Stimecuda'] = Stimecuda
             savez_dict['stdcuda'] = stdcuda
@@ -117,6 +238,55 @@ def collect_results(testname, platform, nruns):
     np.savez(path+testname + "_" + platform + "_nruns" + str(nruns) + ".npz", **savez_dict)
     return
 
+def collect_nsys_results(testname, platform, nruns, output_file):
+    nruns = int(nruns)
+
+    sizes = array('d',[1000, 10000, 100000,1000000,5000000,10000000, 50000000,100000000]) #10,100,1000,10000,100000,
+    Ssizes = array('d',[1000, 10000, 100000,1000000,5000000,10000000, 50000000,100000000,200000000]) #10,100,1000,10000,100000,
+
+    val = list(sizes)
+    sizes_gb = [v*8*4/1e9 for v in val] #(nb of doubles)*(bytes for doubles)*(4=dim of LVector)/(bytes in GB)
+    sizes_gb = array('d', sizes_gb)
+
+    val = list(Ssizes)
+    Ssizes_gb = [v*4*4/1e9 for v in val] #(nb of floats)*(bytes for floats)*(4=dim of LVector)/(bytes in GB)
+    Ssizes_gb = array('d', Ssizes_gb)
+
+    platform = platform.lower()
+
+    if ('buffers' in platform) or ('buf' in platform) or ('bf' in platform):
+        memtype = 'BUF'
+    elif ('device_pointers' in platform) or ('devptr' in platform) or ('dp' in platform):
+        memtype = 'PTR'
+    else:
+        memtype = ''
+    
+    if (platform == 'cpu'):
+        run_nsys_benchmark(buildacpp, sizes, memtype = memtype, testname = testname, nruns = nruns, btype = 'cpu', output_file = output_file)
+        run_nsys_benchmark(buildacpp, Ssizes, memtype = memtype, precision = 32, testname = testname, nruns = nruns, btype = 'cpu', output_file = output_file)
+    else:
+        if ("cuda" in platform):
+            run_nsys_benchmark(buildcuda, sizes, testname = testname, nruns = nruns, btype = 'cuda', output_file = output_file)
+            run_nsys_benchmark(buildcuda, Ssizes, precision = 32,  nruns = nruns, testname = testname, btype = 'cuda', output_file = output_file)
+        #endif
+
+        run_nsys_benchmark(buildacpp, sizes, memtype = memtype, testname = testname, nruns = nruns, btype = 'sycl', output_file = output_file)
+        run_nsys_benchmark(buildacpp, Ssizes, memtype = memtype, precision = 32,  testname = testname, nruns = nruns, btype = 'sycl', output_file = output_file)
+        
+        if (platform != "hip"):
+            run_nsys_benchmark(buildoneapi, sizes, memtype = memtype, testname = testname, nruns = nruns, btype = 'sycl', output_file = output_file)
+            run_nsys_benchmark(buildoneapi, Ssizes, memtype = memtype, precision = 32, testname = testname, nruns = nruns, btype = 'sycl', output_file = output_file)
+        #endif
+
+    #endif
+    #np.savez(path+testname + "_" + platform + "_nruns" + str(nruns) + ".npz", **savez_dict)
+    return
+
 if __name__ == "__main__":
-    collect_results(sys.argv[1],sys.argv[2], sys.argv[3])
+    #collect_results(sys.argv[1],sys.argv[2], sys.argv[3])
+    if len(sys.argv) > 4:
+        output_file = sys.argv[4]
+    else:
+        output_file = f"{time.strftime('%Y%m%d-%H%M%S')}"
+    collect_nsys_results(sys.argv[1],sys.argv[2], sys.argv[3], output_file)
 
--- plots/runtests.sh
+++ plots/runtests.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-python /home/mdessole/Projects/ROOT/genvectorxfork/plots/plots.py Boost cpu 3
-python /home/mdessole/Projects/ROOT/genvectorxfork/plots/plots.py InvariantMasses cpu 3
+python /home/mdessole/Projects/GenVectorX/genvectorx/plots/plots.py Boost cpu 3
+python /home/mdessole/Projects/GenVectorX/genvectorx/plots/plots.py InvariantMasses cpu 3
 
-ACPP_VISIBILITY_MASK=ocl ONEAPI_DEVICE_SELECTOR=opencl:0 python /home/mdessole/Projects/ROOT/genvectorxfork/plots/plots.py Boost fpga_$1 3
-ACPP_VISIBILITY_MASK=ocl ONEAPI_DEVICE_SELECTOR=opencl:1 python /home/mdessole/Projects/ROOT/genvectorxfork/plots/plots.py Boost ocl_$1 3
-ACPP_VISIBILITY_MASK=cuda ONEAPI_DEVICE_SELECTOR=cuda:0 python /home/mdessole/Projects/ROOT/genvectorxfork/plots/plots.py Boost cuda_$1 3
+ACPP_VISIBILITY_MASK=ocl ONEAPI_DEVICE_SELECTOR=opencl:0 python /home/mdessole/Projects/GenVectorX/genvectorx/plots/plots.py Boost fpga_$1 3
+ACPP_VISIBILITY_MASK=ocl ONEAPI_DEVICE_SELECTOR=opencl:1 python /home/mdessole/Projects/GenVectorX/genvectorx/plots/plots.py Boost ocl_$1 3
+ACPP_VISIBILITY_MASK=cuda ONEAPI_DEVICE_SELECTOR=cuda:0 python /home/mdessole/Projects/GenVectorX/genvectorx/plots/plots.py Boost cuda_$1 3
 
-ACPP_VISIBILITY_MASK=ocl ONEAPI_DEVICE_SELECTOR=opencl:1 python /home/mdessole/Projects/ROOT/genvectorxfork/plots/plots.py InvariantMasses ocl_$1 3
-ACPP_VISIBILITY_MASK=ocl ONEAPI_DEVICE_SELECTOR=opencl:0 python /home/mdessole/Projects/ROOT/genvectorxfork/plots/plots.py InvariantMasses fpga_$1 3
-ACPP_VISIBILITY_MASK=cuda ONEAPI_DEVICE_SELECTOR=cuda:0 python /home/mdessole/Projects/ROOT/genvectorxfork/plots/plots.py InvariantMasses cuda_$1 3
+ACPP_VISIBILITY_MASK=ocl ONEAPI_DEVICE_SELECTOR=opencl:1 python /home/mdessole/Projects/GenVectorX/genvectorx/plots/plots.py InvariantMasses ocl_$1 3
+ACPP_VISIBILITY_MASK=ocl ONEAPI_DEVICE_SELECTOR=opencl:0 python /home/mdessole/Projects/GenVectorX/genvectorx/plots/plots.py InvariantMasses fpga_$1 3
+ACPP_VISIBILITY_MASK=cuda ONEAPI_DEVICE_SELECTOR=cuda:0 python /home/mdessole/Projects/GenVectorX/genvectorx/plots/plots.py InvariantMasses cuda_$1 3
