3523f269c3c437918aebe46dd8cbfb8bf32388ec
Simone Balducci <simone.balducci00@gmail.com>
2025-02-04 15:58:21 +0100

    Add first cuda benchmark
    Add cuda invariant mass benchmark and fix some syntax
    
    Link google bench to cuda execs
    
    Add missing ranges in cuda benches and small fixes
    
    Use real time in CUDA benches

--- benchmarks/Boost.cpp
+++ benchmarks/Boost.cpp
@@ -31,15 +31,15 @@ static void BM_ApplyBoost(benchmark::State &state) {
   for (auto _ : state) {
     const auto N = state.range(0);
     auto lvectors = GenVectors(N);
-    auto* lvectorsboost = new LVector[N];
+    auto* lvectorsboosted = new LVector[N];
 
     Boost bst(0.3, 0.4, 0.5);
-	lvectorsboost = ROOT::Experimental::ApplyBoost(lvectors.get(), bst, N);
+	lvectorsboosted = ROOT::Experimental::ApplyBoost(lvectors.get(), bst, N);
 
-	delete[] lvectorsboost;
+	delete[] lvectorsboosted;
   }
 }
 
-BENCHMARK(BM_ApplyBoost)->RangeMultiplier(2)->Range(1 << 10, 1 << 25);
+BENCHMARK(BM_ApplyBoost)->RangeMultiplier(2)->Range(1 << 10, 1 << 20);
 
 BENCHMARK_MAIN();
