e4996d6b4025554bf2618cf3a68f72ffe5cfc7a4
Simone Balducci <simone.balducci00@gmail.com>
2025-02-04 15:58:21 +0100

    Add first cuda benchmark

---
+++ benchmarks/Boost.cu
@@ -0,0 +1,54 @@
+#include "SYCLMath/Boost.h"
+#include "SYCLMath/GenVector/MathUtil.h"
+#include "SYCLMath/VecOps.h"
+#include "SYCLMath/Vector4D.h"
+#include <benchmark/benchmark.h>
+#include <cuda_runtime.h>
+#include <vector>
+
+#ifdef SINGLE_PRECISION
+using Scalar = float;
+#else
+using Scalar = double;
+#endif
+
+using LVector =
+    ROOT::Experimental::LorentzVector<ROOT::Experimental::PxPyPzE4D<Scalar>>;
+using Boost = ROOT::Experimental::Boost;
+
+template <class T> using Vector = std::vector<T>; // ROOT::RVec<T>;
+
+auto GenVectors(int n) {
+  auto vectors = std::make_unique<LVector[]>(n);
+
+  // generate n -4 momentum quantities
+  std::for_each(vectors.get(), vectors.get() + n,
+                [](auto &vec) -> void { vec = {1., 1., 1., 1.}; });
+
+  return std::move(vectors);
+}
+
+static void BM_ApplyBoost(benchmark::State &state) {
+  for (auto _ : state) {
+    const auto N = state.range(0);
+    size_t local_size = 128;
+
+    int count;
+    cudaGetDeviceCount(&count);
+    cudaSetDevice(count - 1);
+
+    auto lvectors = GenVectors(N);
+    LVector *lvectorsboosted = new LVector[N];
+
+    Boost bst(0.3, 0.4, 0.5);
+
+    lvectorsboosted =
+        ROOT::Experimental::ApplyBoost<Boost, LVector>(lvectors.get(), bst, N, local_size);
+
+    delete[] lvb;
+  }
+}
+
+BENCHMARK(BM_ApplyBoost);
+
+BENCHMARK_MAIN();
