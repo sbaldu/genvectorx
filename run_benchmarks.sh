c2512358d7c0472380531c5d8e6d0ac4e31b1688
Simone <simone.balducci00@gmail.com>
2025-02-12 15:59:07 +0100

    Update benchmark script

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -1,10 +1,28 @@
 #
 
-./build/benchmarks/Boost_bench --benchmark_out=test.csv 			   \
-							   --benchmark_out_format=csv			   \
-							   --benchmark_report_aggregates_only=true \
-							   --benchmark_repetitions=5
-./build/benchmarks/InvariantMass_bench --benchmark_out=test.csv 			   \
-									   --benchmark_out_format=csv			   \
-									   --benchmark_report_aggregates_only=true \
-									   --benchmark_repetitions=5
+./build/benchmarks/Boost_bench --benchmark_out=Boost_bench.json 			   \
+							   --benchmark_out_format=json			           \
+							   --benchmark_report_aggregates_only=true         \
+							   --benchmark_repetitions=3
+./build/benchmarks/InvariantMass_bench --benchmark_out=InvMass_bench.json			   \
+									   --benchmark_out_format=json		               \
+									   --benchmark_report_aggregates_only=true         \
+									   --benchmark_repetitions=3
+
+./build/benchmarks/BoostCuda_bench --benchmark_out=Boost_bench.json			   \
+							   --benchmark_out_format=json		               \
+							   --benchmark_report_aggregates_only=true         \
+							   --benchmark_repetitions=3
+./build/benchmarks/InvariantMassCuda_bench --benchmark_out=InvMass_bench.json			   \
+									   --benchmark_out_format=json		                   \
+									   --benchmark_report_aggregates_only=true             \
+									   --benchmark_repetitions=3
+
+./build/benchmarks/BoostCudaStreamed_bench --benchmark_out=BoostCudaStreamed_bench.json			   \
+							   --benchmark_out_format=json		                                   \
+							   --benchmark_report_aggregates_only=true                             \
+							   --benchmark_repetitions=3
+./build/benchmarks/InvariantMassCudaStreamed_bench --benchmark_out=InvMassCudaStreamed_bench.json	    \
+                                                   --benchmark_out_format=json		                    \
+                                                   --benchmark_report_aggregates_only=true              \
+                                                   --benchmark_repetitions=3
